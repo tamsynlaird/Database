6f69970867886d2d006de24b2373b0dbb9144ff2
tl8196 <tl8196@onelearn.nz>
2021-11-17 15:27:47 +1300

    save

--- landf/views.py
+++ landf/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Lost
+from datetime import datetime, timedelta
 
 def index(request):
     
-    all_lost = Lost.objects.all()
+    all_lost = Lost.objects.all().order_by('date')
 
-    context = {'all_lost':all_lost}
+    lost_phone = all_lost.filter(title__contains = 'phone')
+
+    lost_today = all_lost.filter(date = datetime.now())
+
+    context = {'all_lost':all_lost, 'lost_phone':lost_phone, 'lost_today':lost_today}
 
     return render(request, 'landf/index.html', context)
